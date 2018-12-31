a38e762a8084c8a004d6b8bec49da1b273865a1b
abc <abc@abc.abc>
2018-12-30 22:39:10 -0800

    no message

--- src/main/java/s0151/ST0159.java
+++ src/main/java/s0151/ST0159.java
@@ -18,4 +18,30 @@ Output: 5
 Explanation: t is "aabbb" which its length is 5.
  */
 public class ST0159 {
+    public int lengthOfLongestSubstringTwoDistinct(String s) {
+        char a=0, b=0, c;
+        int aCount=0, bCount=0, count=0, max=0;
+        for (int i = 0; i < s.length(); i++) {
+            c = s.charAt(i);
+            if (c==b) {
+                count++;
+                bCount++;
+            } else if (c==a) {
+                count++;
+                aCount++;
+                a=b;
+                b=c;
+                aCount=bCount;
+                bCount=1;
+            } else {
+                a=b;
+                b=c;
+                aCount=bCount;
+                count = bCount+1;
+                bCount=1;
+            }
+            max = Math.max(max, count);
+        }
+        return max;
+    }
 }
